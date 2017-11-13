1e51af2e7752dfdc84558f198353d0b46bdddb02
Gennon <imgennon@gmail.com>
2017-11-13 14:55:02 +0100

    Started on the CLI implementation.

---
+++ README.md
@@ -0,0 +1,23 @@
+# Readme
+
+## Description
+
+This tool is made to make it convenient to transfer Visual Studio Code extensions from an online computer to an offline computer. It basically just downloads all your installed extensions from the online computer and puts them in a folder or archive. The files are vsix which is easy to install using Visual Studio Code (Go to Extensions -> Install from VSIX...).
+
+As an option you can select to download the extensions into a folder structure like
+
+* Publisher
+  * Extension
+    * Version
+
+This is handy when using on-premise repository tools like Artifacory.
+
+## Usage
+
+### Install
+
+### Download VSIX files into a flat structure
+
+### Download VSIX files into a folder structure
+
+### Zip it up
---
+++ main.js
@@ -0,0 +1,26 @@
+const download = require('./lib/downloader').download;
+const program = require('commander');
+const pkg = require('./package.json');
+
+program
+  .version(pkg.version)
+  .option('-f, --flat', 'Save the VSIX in a flat structure, othwerwise makes use of Publisher/Extension/Version structure. [true]')
+  .option('-a, --archive', 'Archive the final folder as a tar ball. [false]')
+  .option('-v, --verbose', 'Prints detailed progress. [false]')
+  .parse(process.argv);
+
+if(program.flat === undefined){
+  program.flat = true;
+}
+
+if(program.archive === undefined){
+  program.archive = false;
+}
+
+if(program.verbose === undefined){
+  program.verbose = false;
+}
+
+const { archive, flat, verbose } = program;
+
+download({archive, flat, verbose});
